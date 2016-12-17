c7efb36a0f555379da6dc43c158fc5e2148b0aab
Michal Kokocinski <michal@waat.eu>
2016-12-17 01:12:29 +0100

    Finish create_component script with usage of gnu-sed

--- purge_styleguide.sh
+++ purge_styleguide.sh
@@ -8,15 +8,15 @@ rm -rf ./styleguide
 rm ./sass/styleguide-overrides.sass
 
 echo "Removing styleguide generation tasks from Gulp"
-sed -i '' 's/"gulp-watch": "~4.3.5",/"gulp-watch": "~4.3.5"/' package.json
-sed -i '' "/sc5-styleguide/d" package.json
+sed -i 's/"gulp-watch": "~4.3.5",/"gulp-watch": "~4.3.5"/' package.json
+sed -i "/sc5-styleguide/d" package.json
 
-sed -i '' "/styleguide = require/d" gulpfile.js
-sed -i '' "23,30d" gulpfile.js
-sed -i '' "47,89d" gulpfile.js
-sed -i '' "/    'styleguide',/d" gulpfile.js
-sed -i '' "/    'images',/d" gulpfile.js
-sed -i '' "/    'js',/d" gulpfile.js
-sed -i '' "s/paths\.styleguide\.html, //g" gulpfile.js
+sed -i "/styleguide = require/d" gulpfile.js
+sed -i "23,30d" gulpfile.js
+sed -i "47,89d" gulpfile.js
+sed -i "/    'styleguide',/d" gulpfile.js
+sed -i "/    'images',/d" gulpfile.js
+sed -i "/    'js',/d" gulpfile.js
+sed -i "s/paths\.styleguide\.html, //g" gulpfile.js
 
 echo "Done"
