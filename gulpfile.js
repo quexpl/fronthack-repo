8ead2f17dc416537b46eaf5e064c65ced30b2629
Michal Kokocinski <michal@waat.eu>
2017-02-20 15:22:21 +0100

    Remove styleguide from gh-pages branch

--- gulpfile.js
+++ gulpfile.js
@@ -53,67 +53,15 @@ gulp.task('mustache', function() {
     .pipe(gulp.dest(paths.mustache.output));
 });
 
-// Define rendering styleguide task
-// https://github.com/SC5/sc5-styleguide#build-options
-gulp.task('styleguide:generate', function() {
-  return gulp.src(paths.styleguide.sass)
-    .pipe(styleguide.generate({
-        title: 'Fronthack styleguide',
-        server: true,
-        sideNav: true,
-        rootPath: paths.styleguide.output,
-        appRoot: '/fronthack/styleguide',
-        overviewPath: 'README.md',
-        commonClass: 'body',
-        extraHead: [
-          '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>',
-          '<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.0/owl.carousel.min.js"></script>',
-          '<script src="/js/components/owl-carousel.js"></script>',
-        ],
-        disableEncapsulation: true
-      }))
-    .pipe(gulp.dest(paths.styleguide.output));
-});
-gulp.task('styleguide:applystyles', function() {
-  return gulp.src([
-      'sass/app.sass',
-      'sass/styleguide-overrides.sass'
-    ])
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(styleguide.applyStyles())
-    .pipe(gulp.dest(paths.styleguide.output));
-});
-gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
-
-// Define copying images for styleguide task
-gulp.task('images', function() {
-  gulp.src(['images/**'])
-    .pipe(gulp.dest(paths.styleguide.output + '/images'));
-});
-
-// Define copying javascript for styleguide task
-gulp.task('js', function() {
-  gulp.src(['js/components/**'])
-    .pipe(gulp.dest(paths.styleguide.output + '/js/components'));
-});
-
 
 // Listen folders for changes and apply defined tasks
 gulp.task('default', [
-    'styleguide',
     'sass',
-    'images',
-    'js',
     'mustache'
   ], function() {
   livereload.listen();
   gulp.watch([paths.sass.allfiles, paths.styleguide.html, paths.mustache.allfiles], [
-    'styleguide',
     'sass',
-    'images',
-    'js',
     'mustache'
   ]);
 });
