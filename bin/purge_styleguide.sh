b9e6ec532833feea6126090928594260d232296a
Michal Kokocinski <michal@waat.eu>
2017-02-21 19:18:24 +0100

    Fix purge_styleguide script

--- bin/purge_styleguide.sh
+++ bin/purge_styleguide.sh
@@ -8,12 +8,12 @@ rm -rf ./styleguide
 rm ./sass/styleguide-overrides.sass
 
 echo "Removing styleguide generation tasks from Gulp"
-sed -i 's/"gulp-watch": "~4.3.5",/"gulp-watch": "~4.3.5"/' package.json
+sed -i 's/"gulp-watch": "~4.3.11",/"gulp-watch": "~4.3.11"/' package.json
 sed -i "/sc5-styleguide/d" package.json
 
 sed -i "/styleguide = require/d" gulpfile.js
-sed -i "23,30d" gulpfile.js
-sed -i "47,89d" gulpfile.js
+sed -i "22,29d" gulpfile.js
+sed -i "46,90d" gulpfile.js
 sed -i "/    'styleguide',/d" gulpfile.js
 sed -i "/    'images',/d" gulpfile.js
 sed -i "/    'js',/d" gulpfile.js
