9129abaa279352762ca794c46cf95a01214d5a9c
Michal Kokocinski <michal@waat.eu>
2017-04-11 17:38:12 +0200

    Update purge_styleguide script to remove unneccesary stuff from sass files

--- src/bin/purge_styleguide.sh
+++ src/bin/purge_styleguide.sh
@@ -9,6 +9,17 @@ rm ./src/sass/styleguide-overrides.sass
 rm -rf ./src/sass/base/html
 rm -rf ./src/sass/components/html
 
+echo "Removing unneccessary stuff from Sass files"
+for file in $(ls ./src/sass/*/*.sass)
+do
+  sed -i "/^\/\/ Markup:/d" $file
+  sed -i "/^\/\/ default/d" $file
+  sed -i "/^\/\/ \./d" $file
+  sed -i "/^\/\/ Styleguide/d" $file
+  sed -i "/^\/\/ sg-wrapper/d" $file
+  sed -i "/^\/\/ <div/d" $file
+done
+
 echo "Removing styleguide generation tasks from Gulp"
 sed -i 's/"gulp-watch": "~4.3.11",/"gulp-watch": "~4.3.11"/' package.json
 sed -i "/sc5-styleguide/d" package.json
